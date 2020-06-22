5c3616284621bbb142a3a465899a3ee008ee1a90
dylanfisher <hi@dylanfisher.com>
2020-06-22 15:26:33 -0400

    Specify forest branch v2

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 ruby '2.7.1'
 
 # gem 'forest', git: 'https://github.com/dylanfisher/forest.git'
-gem 'forest', path: '~/projects/forest'
+gem 'forest', path: '~/projects/forest', branch: 'v2'
 gem 'rails', '~> 6.0'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 4.1'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,11 +16,14 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, max-age=31536000'
+  }
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -36,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -46,7 +49,13 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  # Use a different cache store in production.
+  if ENV['MEMCACHEDCLOUD_SERVERS']
+    config.cache_store = :mem_cache_store,
+                         ENV['MEMCACHEDCLOUD_SERVERS'].split(','), {
+                           username: ENV['MEMCACHEDCLOUD_USERNAME'],
+                           password: ENV['MEMCACHEDCLOUD_PASSWORD'] }
+  end
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
